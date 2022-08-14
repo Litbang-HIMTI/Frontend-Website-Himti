77a162a1746d5afe15ca61ae6f635ad9669ef785
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-08-14 16:00:29 +0700

    refactor: notes
    - add placeholder to api so the folder get track to git

--- src/components/Admin/Note/Note.tsx
+++ src/components/Admin/Note/Note.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { IDashboardProps } from "../../interfaces/props/Dashboard";
+import { IDashboardProps } from "../../../interfaces/props/Dashboard";
 
 export const Note: NextPage<IDashboardProps> = (props) => {
 	return <h1>Dashboard Note</h1>;
