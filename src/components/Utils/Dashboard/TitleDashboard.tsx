1daeb72fff9aba857c610107f3a2df43b23868b9
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-08-16 20:49:41 +0700

    feat: add link breadcrumbs

--- src/components/Utils/Dashboard/TitleDashboard.tsx
+++ src/components/Utils/Dashboard/TitleDashboard.tsx
@@ -1,18 +1,60 @@
-import { Button } from "@mantine/core";
+import { Breadcrumbs, Button, Text } from "@mantine/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { TablerIcon, IconFilePlus } from "@tabler/icons";
+import { useEffect, useState } from "react";
 
 export const TitleDashboard = ({ title, hrefLink, hrefText, HrefIcon = IconFilePlus, mb }: { title: string; hrefLink?: string; hrefText?: string; HrefIcon?: TablerIcon; mb?: string }) => {
+	const router = useRouter();
+	const [links, setLinks] = useState<any[]>([]);
+	const getPath = () => {
+		return router.pathname
+			.split("/")
+			.map((item) => {
+				const path = item.replace("[", "").replace("]", "");
+				return item.includes("[") ? router.query[path] : item.charAt(0).toUpperCase() + item.slice(1);
+			})
+			.filter((item) => item);
+	};
+
+	useEffect(() => {
+		setLinks(getPath());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
 	return (
-		<div className="dash-flex" style={{ marginBottom: mb ? mb : "1.5rem" }}>
-			<h1>{title}</h1>
-			{hrefLink && hrefText && (
-				<Link href={hrefLink}>
-					<Button id="dash-add-new" ml={16} size="xs" compact leftIcon={<HrefIcon size={20} />}>
-						{hrefText}
-					</Button>
-				</Link>
-			)}
+		<div className="dash-flex-column" style={{ marginBottom: mb ? mb : "1.5rem" }}>
+			<div className="dash-flex">
+				<h1>{title}</h1>
+				{hrefLink && hrefText && (
+					<Link href={hrefLink}>
+						<Button id="dash-add-new" ml={16} size="xs" compact leftIcon={<HrefIcon size={20} />}>
+							{hrefText}
+						</Button>
+					</Link>
+				)}
+			</div>
+			<Breadcrumbs mt={6}>
+				{links.length > 1 &&
+					links.map((item, index) => {
+						return (
+							<Link
+								key={index}
+								href={
+									"/" +
+									links
+										.slice(0, index + 1)
+										.join("/")
+										.toLowerCase()
+								}
+							>
+								<a>
+									<Text variant="link">{item}</Text>
+								</a>
+							</Link>
+						);
+					})}
+			</Breadcrumbs>
 		</div>
 	);
 };
